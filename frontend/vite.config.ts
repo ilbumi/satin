25c7c94181145104e7ddbc30906a899be65aa52c
Ilia Bushmakin <im.bushmakin@gmail.com>
2025-08-22 16:31:41 +0400

    fix: fix tests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
